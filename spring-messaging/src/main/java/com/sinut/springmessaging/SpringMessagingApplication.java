1aaf2cd8d7a02480030439e48f998bb27ed0503e
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2023-05-05 20:07:12 +0700

    feat: msg controller 🔥

---
+++ spring-messaging/src/main/java/com/sinut/springmessaging/SpringMessagingApplication.java
@@ -0,0 +1,32 @@
+package com.sinut.springmessaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMessagingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMessagingApplication.class, args);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ spring-messaging/src/main/java/com/sinut/springmessaging/model/Message.java
@@ -0,0 +1,12 @@
+package com.sinut.springmessaging.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Message {
+    private String content;
+}
