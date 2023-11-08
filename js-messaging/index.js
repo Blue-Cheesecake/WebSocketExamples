78ab984c5098aec3fede68c286ac35a6dc5a0d80
Sinut Wattanarporn <s_w.piepie@outlook.com.au>
2023-11-08 23:16:49 +0700

    feat: WEBS-0 js receive data from client

--- js-messaging/index.js
+++ js-messaging/index.js
@@ -10,6 +10,10 @@ console.log("Running WebSocket Server on Port:", port);
 wss.on("connection", (ws) => {
   console.log("New client connected!");
 
+  ws.on("message", (data) => {
+    console.log("Got data from client", data);
+  });
+
   ws.on("close", () => {
     console.log("Client has disconnected");
   });
