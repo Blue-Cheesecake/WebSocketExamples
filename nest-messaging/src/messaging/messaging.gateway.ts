784d55b503bd91bcc1b13aaf04a396002225d730
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2024-01-20 16:59:44 +0700

    feat: emit, handle message event

--- nest-messaging/src/messaging/messaging.gateway.ts
+++ nest-messaging/src/messaging/messaging.gateway.ts
@@ -1,9 +1,34 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  MessageBody,
+  OnGatewayConnection,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server } from 'socket.io';
 
-@WebSocketGateway({ namespace: 'message-event' })
-export class MessagingGateway {
+@WebSocketGateway()
+export class MessagingGateway implements OnGatewayConnection {
+  @WebSocketServer()
+  server: Server;
+
+  // Emit the message immediately when connection is made
+  handleConnection(client: any, ...args: any[]) {
+    console.log(typeof client);
+    console.log('Connected (GatewatConnection)');
+    let counter = 0;
+    setInterval(() => {
+      this.server.emit('onMessage', {
+        counter: counter,
+      });
+      counter++;
+    }, 1500);
+  }
+
+  // handle the message from client
   @SubscribeMessage('message-event')
-  handleMessage(client: any, payload: any): string {
+  handleMessage(@MessageBody() data: any) {
+    console.log(data);
     return 'Hello world!';
   }
 }
